b9f6394b09f9125910a8673e9b64f5ce4046f6bb
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-27 12:08:14 +0200

    🔧 update: implement start component to app and add animate-presence to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { AnimatePresence, motion } from "framer-motion";
+
+import { Start } from "./components";
+import { useGameStore } from "./util/use-game-store";
+
 function App() {
-  return <div>XO RUMBLE</div>;
+  const { navigate } = useGameStore();
+  return (
+    <AnimatePresence>
+      {navigate === "start" && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Start />
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 }
 
 export default App;
