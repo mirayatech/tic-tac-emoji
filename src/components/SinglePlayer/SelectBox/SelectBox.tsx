9666f2a2805342560d0e6b8f3589a8789969d630
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-27 12:33:00 +0200

    ✨ feat: create select-box component

---
+++ src/components/SinglePlayer/SelectBox/SelectBox.tsx
@@ -0,0 +1,68 @@
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+import { colors } from "../../../assets/variables";
+
+import { CircleIcon, MarkIcon } from "../../../assets/icon";
+
+import { useGameStore } from "../../../util/use-game-store";
+import {
+  Boxes,
+  Button,
+  ButtonsContainer,
+  Card,
+  Divider,
+  OButton,
+  Title,
+  XButton,
+} from "./Styles";
+
+export default function SelectBox() {
+  const { setPlayerSign, setGameNavigate, setNavigate, playerSign } =
+    useGameStore();
+  const [selectedSign, setSelectedSign] = useState<"" | "X" | "O">("");
+
+  const handleClick = (sign: "X" | "O") => {
+    setPlayerSign(sign);
+    setSelectedSign(sign);
+  };
+
+  return (
+    <Card>
+      <Title>
+        <h1>Select who you want to be:</h1> <Divider />
+      </Title>
+      <Boxes>
+        <XButton
+          onClick={handleClick.bind(null, "X")}
+          aria-label="Player X"
+          selected={selectedSign === "X"}
+          shadowColor={colors.redShadow}
+        >
+          <MarkIcon />
+        </XButton>
+
+        <span>OR</span>
+
+        <OButton
+          onClick={handleClick.bind(null, "O")}
+          aria-label="Player O"
+          selected={selectedSign === "O"}
+          shadowColor={"#ffcf339e"}
+        >
+          <Icon icon={CircleIcon} />
+        </OButton>
+      </Boxes>
+      <ButtonsContainer>
+        <Button aria-label="back to home" onClick={() => setNavigate("start")}>
+          Back
+        </Button>
+        <Button
+          disabled={!playerSign}
+          onClick={() => setGameNavigate("single-player-board")}
+        >
+          Play
+        </Button>
+      </ButtonsContainer>
+    </Card>
+  );
+}
---
+++ src/components/SinglePlayer/SelectBox/Styles.tsx
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+import { colors } from "../../../assets/variables";
+
+export const Card = styled.div`
+  gap: 25px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: ${colors.white};
+  box-shadow: ${colors.shadow};
+
+  @media screen and (min-width: 500px) {
+    width: 400px;
+  }
+`;
+
+export const Title = styled.div`
+  h1 {
+    text-align: center;
+    font-size: calc(20 / 16 * 1rem);
+    font-weight: 700;
+  }
+`;
+
+export const Divider = styled.div`
+  margin-top: 8px;
+  padding: 1px;
+  background-color: ${colors.grey};
+`;
+
+export const Boxes = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  span {
+    padding: 0 10px;
+    font-weight: bold;
+    font-size: 25px;
+  }
+`;
+
+const BaseButton = styled.button<{ selected?: boolean; shadowColor: string }>`
+  color: ${colors.white};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  background-color: transparent;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  transform: ${(props) => (props.selected ? "scale(1.05)" : "none")};
+  filter: ${(props) =>
+    props.selected ? `drop-shadow(0 5px 10px ${props.shadowColor})` : "none"};
+  transition: transform 0.3s ease, filter 0.3s ease;
+`;
+
+export const XButton = styled(BaseButton)<{ selected?: boolean }>`
+  font-size: 80px;
+  color: ${colors.red};
+`;
+
+export const OButton = styled(BaseButton)<{ selected?: boolean }>`
+  color: ${colors.yellow};
+  svg {
+    font-size: 100px;
+  }
+`;
+
+export const Button = styled.button`
+  height: 45px;
+  margin: 0 auto;
+  font-weight: bold;
+  color: ${colors.white};
+  border: none;
+  font-size: 1.1rem;
+  max-width: 300px;
+  width: 100%;
+  color: ${colors.white};
+  font-size: 1.1rem;
+  transition: all 200ms ease-in;
+
+  border-radius: 10px;
+  box-shadow: ${colors.shadow};
+
+  &:hover {
+    opacity: 0.9;
+  }
+
+  &:last-child {
+    text-shadow: 0px -2px ${colors.greenBorder};
+    background-color: ${colors.green};
+    border-bottom: 4px solid ${colors.greenBorder};
+  }
+
+  &:first-child {
+    text-shadow: 0px -2px ${colors.blueBorder};
+    background-color: ${colors.blue};
+    border-bottom: 4px solid ${colors.blueBorder};
+  }
+
+  &:active {
+    transform: translateY(2px);
+    border-bottom-width: 2px;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  gap: 15px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  @media screen and (min-width: 500px) {
+    flex-direction: row;
+  }
+`;
