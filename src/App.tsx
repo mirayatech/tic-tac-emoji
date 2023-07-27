e2fddf8d801c255ba744aac0ff1bf8c28f082145
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-27 12:34:23 +0200

    🔧 update: implement single-player component to app and add animate-presence to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion";
+import { Suspense } from "react";
 
-import { Start } from "./components";
+import { SinglePlayer, Start } from "./components";
 import { useGameStore } from "./util/use-game-store";
 
 function App() {
@@ -14,6 +15,18 @@ function App() {
           exit={{ opacity: 0 }}
         >
           <Start />
+        </motion.div>
+      )}
+
+      {navigate === "single-player" && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Suspense fallback="loading...">
+            <SinglePlayer />
+          </Suspense>
         </motion.div>
       )}
     </AnimatePresence>
