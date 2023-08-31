95b4af522f5d090cc0995fa4247e2c78d8b5dcd1
〽️iraya <mirayaabrodi@gmail.com>
2023-08-31 05:05:14 +0200

    refactor: chage fnction name

--- src/util/check-winner-service.ts
+++ src/util/check-winner-service.ts
@@ -9,7 +9,7 @@ const winningCombinations = [
   [2, 4, 6],
 ];
 
-export function checkWinner(
+export function checkSinglePlayerWinner(
   board: ("X" | "O" | null)[]
 ): ["X" | "O" | "draw" | null, number[] | null] {
   for (const combo of winningCombinations) {
@@ -25,3 +25,29 @@ export function checkWinner(
 function isDraw(board: ("X" | "O" | null)[]): boolean {
   return !board.includes(null);
 }
+
+export const checkMultiPlayerWinner = (squares: ("X" | "O" | null)[]) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  if (squares.every((square) => square)) {
+    return "draw";
+  }
+
+  return null;
+};
