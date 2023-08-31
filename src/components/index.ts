da8174d2044ea39a19487769e175ec2c36619e25
〽️iraya <mirayaabrodi@gmail.com>
2023-08-31 08:11:13 +0200

    refcator: export multiplayer by name

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ import { lazy } from "react";
 
 export const Start = lazy(() => import("./Start/Start"));
 export const SinglePlayer = lazy(() => import("./SinglePlayer/SinglePlayer"));
+export const MultiPlayer = lazy(() => import("./MultiPlayer/MultiPlayer"));
