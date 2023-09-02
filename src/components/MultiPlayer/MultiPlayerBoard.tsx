9b06b553e210ff1266fc2efce36734d8d24fb27b
〽️iraya <mirayaabrodi@gmail.com>
2023-09-02 10:24:01 +0200

    add: framermotion to multiplayer

--- src/components/MultiPlayer/MultiPlayerBoard.tsx
+++ src/components/MultiPlayer/MultiPlayerBoard.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useGameStore } from "../../util/useGameStore";
 import useMultiplayerStore from "../../util/useMultiplayerStore";
 import { Banner, Board } from "../core";
+import { motion } from "framer-motion";
 
 export default function MultiPlayerBoard() {
   const { setGameNavigate } = useGameStore();
@@ -23,13 +24,18 @@ export default function MultiPlayerBoard() {
   }, [multiplayerWinner, isDraw]);
 
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      exit={{ opacity: 0 }}
+    >
       <Banner
         isPlayerXNext={multiplayerPlayerXIsNext}
         playerSign={emojiX}
         otherPlayerSign={emojiO}
       />
       <Board squares={multiplayerBoard} onClick={multiplayerHandleClick} />
-    </>
+    </motion.div>
   );
 }
--- src/components/MultiPlayer/MultiPlayerResult.tsx
+++ src/components/MultiPlayer/MultiPlayerResult.tsx
@@ -1,19 +1,27 @@
 import { useGameStore } from "../../util/useGameStore";
 import useMultiplayerStore from "../../util/useMultiplayerStore";
 import { Result } from "../core";
+import { motion } from "framer-motion";
 
 export default function MultiplayerResult() {
   const { multiplayerWinner, reset } = useMultiplayerStore();
   const { setNavigate, setGameNavigate } = useGameStore();
 
   return (
-    <Result
-      multiplayerWinner={multiplayerWinner}
-      reset={reset}
-      setNavigate={setNavigate}
-      setGameNavigate={setGameNavigate}
-      isMultiplayer={true}
-      gameResult={null}
-    />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      exit={{ opacity: 0 }}
+    >
+      <Result
+        multiplayerWinner={multiplayerWinner}
+        reset={reset}
+        setNavigate={setNavigate}
+        setGameNavigate={setGameNavigate}
+        isMultiplayer={true}
+        gameResult={null}
+      />
+    </motion.div>
   );
 }
