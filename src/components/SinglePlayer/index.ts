0f592fedf469fa6fcc8565eea707f7f6cb729537
〽️iraya <mirayaabrodi@gmail.com>
2023-09-02 08:50:19 +0200

    fix: rename file

--- src/components/SinglePlayer/index.ts
+++ src/components/SinglePlayer/index.ts
@@ -1,5 +1,5 @@
 import { lazy } from "react";
 
 export const SelectBox = lazy(() => import("./select/select"));
-export const SinglePlayerBoard = lazy(() => import("./singleplayerBoard"));
-export const SinglePlayerResult = lazy(() => import("./singleplayerResult"));
+export const SinglePlayerBoard = lazy(() => import("./SinglePlayerBoard"));
+export const SinglePlayerResult = lazy(() => import("./SinglePlayerResult"));
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import { lazy } from "react";
 
 export const Start = lazy(() => import("./Start/Start"));
-export const SinglePlayer = lazy(() => import("./singleplayer/singleplayer"));
+export const SinglePlayer = lazy(() => import("./SinglePlayer/SinglePlayer"));
 export const MultiPlayer = lazy(() => import("./MultiPlayer/MultiPlayer"));
