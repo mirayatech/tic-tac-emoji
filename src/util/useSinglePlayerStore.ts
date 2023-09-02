e538926164ae62232154181d5380de33474a673d
〽️iraya <mirayaabrodi@gmail.com>
2023-09-02 17:32:41 +0200

    add: ard mode state for game mode

--- src/util/useSinglePlayerStore.ts
+++ src/util/useSinglePlayerStore.ts
@@ -3,12 +3,14 @@ import { persist } from "zustand/middleware";
 import { emojis } from "./types";
 
 type SinglePlayerState = {
+  isHardMode: boolean;
   gameResult: "win" | "lose" | "draw" | null;
   playerSign: emojis | null;
   board: Array<emojis | null>;
   setPlayerSign: (value: emojis | null) => void;
   setBoard: (value: Array<emojis | null>) => void;
   setGameResult: (value: "win" | "lose" | "draw" | null) => void;
+  setIsHardMode: (value: boolean) => void;
   reset: () => void;
 };
 
@@ -18,9 +20,11 @@ export const useSinglePlayer = create<SinglePlayerState>()(
       playerSign: null,
       board: Array(9).fill(null),
       gameResult: null,
+      isHardMode: false,
       setPlayerSign: (val) => set({ playerSign: val }),
       setBoard: (val) => set({ board: val }),
       setGameResult: (val) => set({ gameResult: val }),
+      setIsHardMode: (val) => set({ isHardMode: val }),
       reset: () => {
         set({
           playerSign: null,
